ae30efe99e222bc10909c293d4ad4c4a506c6e0f
Yousitox <yousitox@protonmail.com>
2024-11-25 20:42:08 +0000

    probandoalgo

--- src/ejercicio_1/ejercicio1.java
+++ src/ejercicio_1/ejercicio1.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class ejercicio1 {
 
-    // *declaramos el scanner de forma global*//
+    // *declaramos el scanner de forma global* //
     static Scanner escanear = new Scanner(System.in);
 
     // *declaro una funciones para simplificar el sistem.out.print y asi practico*//
 
-    // *Declaro una funcion solo para texto*//
+    // *Declaro una funcion solo para texto* //
     public static void solotexto(String soloText) {
         System.out.println(soloText);
     }
 
-    // *Declaro una funcion para texto + la variable long *//
+    // *Declaro una funcion para texto + la variable long* //
     public static void imprimStyLn(String texto, long cantidad) {
         System.out.println(texto + cantidad);
     }
@@ -22,19 +22,26 @@ public class ejercicio1 {
     // ! Codigo Principal (Funcion MAIN)
     public static void main(String[] args) {
 
-        // *Inicial *//
+        // *Inicial* //
         solotexto("Introduce un Numero, solo positivo");
         long n = escanear.nextLong();
         imprimStyLn("El numero añadido es el: ", n);
         escanear.close();
 
-        /*
-         * Hacer que si añades un numero negatvio tengas que volver a añadir el balor
-         */
+        long resultado = 0;
+        
+        // *Hacer que si añades un numero negatvio tengas que volver a añadir el balor* //
         if(0 > n){
-            solotexto("Tienes que ingresar un numero positivo");
-            
+            solotexto("Tienes que ingresar un numero positivo");  
         }
+
+        // *hacer el bucle para lo de Fibonacci* //
+        for(int i = 0; i < n; i++){
+            resultado = n + i;
+            System.out.println(i + " " + n);
+        }
+
+
 
     }
 
