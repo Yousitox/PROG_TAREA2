60d50759b336904676351833eaae1912eb497c75
Yousitox <yousitox@protonmail.com>
2024-11-26 17:15:18 +0000

    remodelandoelcodigo

--- src/ejercicio_2/ejercicio2.java
+++ src/ejercicio_2/ejercicio2.java
@@ -15,77 +15,15 @@ public class ejercicio2 {
     }
 
     public static void main(String[] args) {
-        /*
-         * // *MAXIMO UN 10X10*
-         * final byte MAXIMO = 10;
-         * 
-         * // *numero de filas*
-         * textT("Introduce una cantidad de numeros para el array, maximo 10: ");
-         * int fla;
-         * fla = scanear.nextInt();
-         * textTYN("Numero de numeros de arrays añadidos: ", fla);
-         * // *Si supera el valor maximo*
-         * while (MAXIMO < fla) {
-         * textT("Solo agregar maximo " + MAXIMO + ", introduce otra vez otro valor: ");
-         * fla = scanear.nextInt();
-         * }
-         * // *Si es negativo*
-         * while (0 > fla) {
-         * textT("Solo agregar numeros positivos, introduce otra vez otro valor: ");
-         * fla = scanear.nextInt();
-         * }
-         * 
-         // ! CODIGO NO USADO
-         * /*
-         * // *el array*
-         * textT("Introduce los numeros del array");
-         * int[] a = new int[n];
-         * for (int i = 0; i < n; i++) {
-         * a[i] = scanear.nextInt();
-         * }
-         * 
-         * for (int i : a) {
-         * System.out.print(i + " ,");
-         * }
-         */
-        // ! HASTA AQUI EL CODIGO NO USASDO
-        /*
-         * // *numero de columnas*
-         * textT("Introduce una cantidad de columnas");
-         * int clum;
-         * clum = scanear.nextInt();
-         * textTYN("Numero de columnas añadidas: ", clum);
-         * // * Si supera el valor maximo*
-         * while (MAXIMO < clum) {
-         * textT("Solo agregar maximo " + MAXIMO + ", introduce otra vez otro valor: ");
-         * clum = scanear.nextInt();
-         * }
-         * while (0 > clum) {
-         * textT("Solo agregar numeros positivos, introduce otra vez otro valor: ");
-         * clum = scanear.nextInt();
-         * }
-         * scanear.close();
-         * 
-         */ // *Bucle para crear las filas y columnas*
-        /*
-         * for (int i = 1; i <= fla; i++) {
-         * for (int j = 1; j <= clum; j++) {
-         * System.out.print("1 ");
-         * }
-         * System.out.println(" ");
-         * }
-         */
 
+        // ************probando nuevo main***************** */
 
-
-         //************probando nuevo main***************** */
-
-         crearMatriz();
-         mostarMatriz();
+        crearMatriz();
+        mostarMatriz();
 
     }
 
-    // * AQUI PROBANDO COSAS CON LAS MATRICES */
+    // * AQUI PROBANDO LAS FUNCIONES DE LAS MATRICES */
     // *Funcion de la matriz*
     public static void crearMatriz() {
         // *MAXIMO UN 10X10*
