772c94c81b5a342561167f5e6ad160c30dfa891a
Yousitox <yousitox@protonmail.com>
2024-11-27 17:56:47 +0000

    guardando

---
+++ src/ejercicio_3/ejercicio3.java
@@ -0,0 +1,13 @@
+package ejercicio_3;
+
+import java.util.Scanner;
+
+public class ejercicio3 {
+
+    static Scanner scanear = new Scanner(System.in);
+
+    
+    public static void main(String[] args) {
+        
+    }
+}
