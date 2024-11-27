bb02a55f06c543f7847b1a3c48728f4a8701eb4b
Yousitox <yousitox@protonmail.com>
2024-11-27 18:48:07 +0000

    base

--- src/ejercicio_3/ejercicio3.java
+++ src/ejercicio_3/ejercicio3.java
@@ -6,8 +6,47 @@ public class ejercicio3 {
 
     static Scanner scanear = new Scanner(System.in);
 
-    
+    //funcion para abrebiar el sistem.out.println
+    public static void textT(String texto) {
+        System.out.println(texto);
+
+    }
+
+
+    public static void jugar() {
+        textT("PRUEBA1");
+
+    }
+
+    public static void configuracion() {
+        textT("PRUEBA2");
+    }
+
+    public static void salir() {
+        textT("PRUEBA3");
+    }
+
     public static void main(String[] args) {
-        
+
+        textT("-----MENU-------");
+
+        textT("1: Jugar");
+
+        textT("2: Configuracion");
+
+        textT("0: Salir");
+
+        switch (scanear.nextInt()) {
+            case 1:
+                jugar();
+                break;
+            case 2:
+                configuracion();
+                break;
+            default:
+                salir();
+                break;
+        }
+
     }
 }
